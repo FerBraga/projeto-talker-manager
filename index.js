d6cf5b64cf44ad79d744b7460523c0e85a1e4161
fer braga <augustofbg@gmail.com>
2022-06-21 16:15:34 -0300

    requsito 2 ok

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ app.get('/talker', async (_req, res) => {
      res.status(200).json(JSON.parse(talkers));
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
+  const talk = JSON.parse(talkers).find((talker) => talker.id === Number(id));
+  if (talk) {
+    return res.status(200).json(talk);
+  }
+   return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
