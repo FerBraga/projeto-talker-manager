5f2ab5963539427de4241da0c23b296f9187ca92
fer braga <augustofbg@gmail.com>
2022-06-21 17:31:37 -0300

    requisito 4 ok

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+function validation(req, res, next) {
+  const { email, password } = req.body; 
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!email.includes('@', '.com')) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -29,7 +46,7 @@ app.get('/talker/:id', async (req, res) => {
    return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
 });
 
-app.post('/login', async (req, res) => {
+app.post('/login', validation, async (req, res) => {
   const token = { token: Math.random().toString().slice(1, 17) };
   const { email, password } = req.body;
   console.log(email, password);
